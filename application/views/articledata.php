1abaf54c4a31cb319b9aa4077418ff7d3368000c
NRV <nrv.rox.u@gmail.com>
2016-06-12 19:06:40 +0530

    Add OnClick method that display contents.

--- application/views/articledata.php
+++ application/views/articledata.php
@@ -1,6 +1,11 @@
 <html><head>
 
+
     <base href="http://localhost/aprj/">
+    <script type="text/javascript" src="newjscss/jquery.js"></script>
+    <script type="text/javascript" src="newjscss/html5gallery.js"></script>
+    <script type="application/javascript" src="newjscss/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="newjscss/bootstrap.min.css">
 </head>
 <body bgcolor="#FFFFF">
 <?php
@@ -14,27 +19,72 @@
 
 //var_dump($alldata[0]);
 if(isset($alldata[0]["title"])){
+
     echo "<h1>".$alldata[0]["title"]."</h1>";
 
 }
+echo "<div class=\"modal-body row\">";
+echo "<div class=\"col-lg-6\">";
+echo "<div style=\"display:none;\" class=\"html5gallery\" data-skin=\"darkness\" data-width=\"480\" data-height=\"272\">";
+
 if(isset($alldata[0]["image"])){
-    echo "<img src=".$alldata[0]["image"].">";
+    $thumbnail=str_replace ( "\\" , "/" , $alldata[0]["image"] );
+    echo "<a href=\"$thumbnail\"><img src=\"$thumbnail\" alt=\"Trees\"></a>";
+
 
 }
 if(isset($alldata[0]["image1"])){
-    echo "<img src=".$alldata[0]["image1"].">";
+    $thumbnail=str_replace ( "\\" , "/" , $alldata[0]["image1"] );
+    echo "<a href=\"$thumbnail\"><img src=\"$thumbnail\" alt=\"Trees\"></a>";
+
 
 }
 if(isset($alldata[0]["image2"])){
-    echo "<img src=".$alldata[0]["image2"].">";
+    $thumbnail=str_replace ( "\\" , "/" , $alldata[0]["image2"] );
+    echo "<a href=\"$thumbnail\"><img src=\"$thumbnail\" alt=\"Trees\"></a>";
+
 
 }
 if(isset($alldata[0]["image3"])){
-    echo "<img src=".$alldata[0]["image3"].">";
+//    echo "<img src=".$alldata[0]["image3"].">";
+    $thumbnail=str_replace ( "\\" , "/" , $alldata[0]["image3"] );
+    echo "<a href=\"$thumbnail\"><img src=\"$thumbnail\" alt=\"Trees\"></a>";
 
 }
+if(isset($alldata[0]["video"])){
+//    echo "<img src=".$alldata[0]["image3"].">";
+    $thumbnail=str_replace ( "\\" , "/" , $alldata[0]["video"] );
+    $thumbnailvideo=str_replace ( "\\" , "/" , $alldata[0]["thumbnail"] );
+    //echo "<a href=\"$thumbnail\"><img src=\"$thumbnail\" alt=\"Trees\"></a>";
+    echo "<a href=\"$thumbnail\"><img src=\"$thumbnailvideo\" alt=\"Vimeo Video\"></a>";
+}
+
 
 ?>
+
+
+
+    <!-- Add videos to Gallery
+    <a href="images/Big_Buck_Bunny.mp4"><img src="images/Big_Buck_Bunny.jpg" alt="Big Buck Bunny, Copyright Blender Foundation"></a>
+
+    <!-- Add Youtube video to Gallery
+    <a href="http://www.youtube.com/embed/YE7VzlLtp-4"><img src="http://img.youtube.com/vi/YE7VzlLtp-4/2.jpg" alt="Youtube Video"></a>
+
+    <!-- Add Vimeo video to Gallery
+    <a href="https://player.vimeo.com/video/1084537?title=0&amp;byline=0&amp;portrait=0"><img src="images/Big_Buck_Bunny.jpg" alt="Vimeo Video"></a>
+-->
+</div>
+</div>
+
+<div class="col-lg-6">
+    <?php
+//hhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+    echo $alldata[0]["content"];
+?>
+        <!-- Your second column here -->
+    </div>
+</div>
+
 
 </body>
 </html>
