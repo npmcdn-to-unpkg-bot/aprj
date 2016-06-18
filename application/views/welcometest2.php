d0271208363b8d6f77169cd714b3f7334e8ee2fa
dinithminura <dinithmsdk0007@gmail.com>
2016-06-18 11:39:01 +0530

    media boxes center position

--- application/views/welcometest2.php
+++ application/views/welcometest2.php
@@ -177,7 +177,7 @@
 <!---->
 
 <!-- The grid with media boxes -->
-<div style="justify-content: center">
+<div style="justify-content: center;position: relative;">
 
 <div id="grid" style="width: auto;">
 
